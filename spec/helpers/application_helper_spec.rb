dcb3fb16e5cce4a280ef24e87b7d86cb04daf8c9
toctan <tianjin.sc@gmail.com>
2013-09-24 23:08:59 +0800

    Do not hit database testing avatar_url helper

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -3,15 +3,18 @@ require "spec_helper"
 describe ApplicationHelper do
   describe "#avatar_url" do
     it "return avatar url for avatar_users" do
-      @user = FactoryGirl.create(:avatar_user)
+      user = double(:avatar? => true)
+      user.stub_chain(:avatar, :url)
+      user.should_receive(:avatar)
 
-      helper.avatar_url(@user).should eq @user.avatar.url(:thumb)
+      helper.avatar_url(user)
     end
 
     it "return gravatar url for user" do
-      @user = FactoryGirl.create(:user)
+      user = double(:avatar? => false, email: "test@me.com")
 
-      helper.avatar_url(@user).should eq "http://gravatar.com/avatar/8867aebd24d5d4809c3ab6fe4a0771e7.png?s=20&f=y&d=mm"
+      helper.avatar_url(user).should eq "http://gravatar.com/avatar/" \
+      "8867aebd24d5d4809c3ab6fe4a0771e7.png?s=20&f=y&d=mm"
     end
   end
 end
