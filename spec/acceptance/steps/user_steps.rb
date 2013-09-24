3c40106e4add99ef626f25ca6a30c752b5ce7093
farmerworking <farmerworking@gmail.com>
2013-09-24 12:00:34 +0800

    Refactory code

--- spec/acceptance/steps/user_steps.rb
+++ spec/acceptance/steps/user_steps.rb
@@ -6,16 +6,38 @@ module UserSteps
   def fill_sign_in_form(login)
     fill_in 'Login',    with: @user.send(login)
     fill_in 'Password', with: @user.password
+
     click_button 'Sign in'
   end
+
+  def fill_sign_up_form
+    fill_in 'Username',    with: visitor[:username]
+    fill_in 'Email', with: visitor[:email]
+    fill_in 'Password', with: visitor[:password]
+
+    click_button 'Sign up'
+  end
+
+  def fill_in_update_form(update_item, password = nil)
+    if password
+      fill_in "Current password", with: @user.password
+    end
+
+    if update_item == 'password'
+      fill_in 'Password', with: @user.password + 'update'
+      fill_in 'Password confirmation', with: @user.password + 'update'
+    else
+      fill_in update_item.capitalize, with: @user.send(update_item) + "update"
+    end
+
+    click_button "Update"
+  end
+
 
   step 'I sign up with valid data' do
     visit '/users/sign_up'
 
-    fill_in 'Username', with: visitor[:username]
-    fill_in 'Email',    with: visitor[:email]
-    fill_in 'Password', with: visitor[:password]
-    click_button 'Sign up'
+    fill_sign_up_form
   end
 
   step 'I click the confirmation link' do
@@ -27,20 +49,12 @@ module UserSteps
     @user = FactoryGirl.create(:confirmed_user)
   end
 
-  step 'I am on sign in page' do
-    visit new_user_session_path
-  end
-
   step 'I sign in with invalid data' do
     click_button 'Sign in'
   end
 
-  step 'I sign in with email' do
-    fill_sign_in_form('email')
-  end
-
-  step 'I sign in with username' do
-    fill_sign_in_form('username')
+  step 'I sign in with :login' do |login|
+    fill_sign_in_form(login)
   end
 
   step 'I am signed in' do
@@ -52,31 +66,20 @@ module UserSteps
     visit '/users/sign_out'
   end
 
+  step 'I am on sign in page' do
+    visit new_user_session_path
+  end
+
   step "I am on update page" do
     visit edit_user_registration_path
   end
 
-  step "I update username without password" do
-    fill_in "Username", with: @user.username + "update"
-    click_button "Update"
+  step "I update :update_item without password" do |update_item|
+    fill_in_update_form(update_item)
   end
 
-  step "I update password without password" do
-    fill_in "Password", with: @user.password + "update"
-    fill_in "Password confirmation", with: @user.password + "update"
-    click_button "Update"
-  end
-
-  step "I update email without password" do
-    fill_in "Email", with: @user.email + "update"
-    click_button "Update"
-  end
-
-  step "I update password with password" do
-    fill_in "Password", with: @user.password + "update"
-    fill_in "Password confirmation", with: @user.password + "update"
-    fill_in "Current password", with: @user.password
-    click_button "Update"
+  step "I update :update_item with password" do |update_item|
+    fill_in_update_form(update_item, true)
   end
 end
 
