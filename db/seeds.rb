4fe4e573cafdd2645ff720f57aeb2bc57a2b9e9f
toctan <tianjin.sc@gmail.com>
2013-09-27 21:18:34 +0800

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,23 +6,28 @@ user = FactoryGirl.create(:confirmed_user,
                           username: 'toctan',
                           email: 'i@example.com')
 
-FactoryGirl.create_list(:topic, 10, user: user)
+p ' > primary node ...'
+
+node = FactoryGirl.create(:node, name: 'fora')
+
+FactoryGirl.create_list(:topic, 10, user: user, node: node)
 
 p ' > topics ...'
 
-FactoryGirl.create_list(:topic, 20)
+FactoryGirl.create_list(:topic, 20, node: node)
 
 p ' > primary topic ...'
 
-topic = FactoryGirl.create(:topic_with_body, user: user)
+topic = FactoryGirl.create(:topic_with_body, user: user, node: node)
 
 FactoryGirl.create_list(:reply, 30, topic: topic)
 
-p ' > primary chinese topic ...'
+p ' > primary Chinese topic ...'
 
 cntopic = FactoryGirl.create(:topic,
                              title: Faker::LoremCN.sentence,
                              body:  Faker::LoremCN.paragraph,
+                             node: node,
                              user: user)
 
 30.times do
