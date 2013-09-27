6c182566b717ada8771be0f99ac20873bcb77012
toctan <tianjin.sc@gmail.com>
2013-09-27 21:18:54 +0800

    Update topic's timestamp upon new reply

--- app/models/reply.rb
+++ app/models/reply.rb
@@ -1,5 +1,5 @@
 class Reply < ActiveRecord::Base
-  belongs_to :topic, counter_cache: true
+  belongs_to :topic, counter_cache: true, touch: true
   belongs_to :user, counter_cache: true
 
   validates_presence_of :body, :topic_id, :user_id
