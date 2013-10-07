c3f7dcdb0da4f5b8e1f3235c0062adb747630b04
toctan <tianjin.sc@gmail.com>
2013-10-07 10:58:54 +0800

    Omniauth with weibo

--- spec/features/users/omniauth_spec.rb
+++ spec/features/users/omniauth_spec.rb
@@ -1,9 +1,15 @@
 require 'spec_helper'
 
-feature 'Visitor signs up via twitter' do
-  scenario 'authorized successfully' do
+feature 'Users get authenticated' do
+  scenario 'via twitter' do
     visit user_omniauth_authorize_url(:twitter)
 
-    expect(page).to have_flash_message 'Successfully authenticated from twitter'
+    expect(page).to have_flash_message 'authenticated from twitter'
+  end
+
+  scenario 'via weibo' do
+    visit user_omniauth_authorize_url(:weibo)
+
+    expect(page).to have_flash_message 'authenticated from weibo'
   end
 end
--- spec/support/omniauth.rb
+++ spec/support/omniauth.rb
@@ -1,10 +1,12 @@
 OmniAuth.config.test_mode = true
 
-OmniAuth.config.mock_auth[:twitter] = OmniAuth::AuthHash.new({
-    provider: 'twitter',
-    uid: '123545',
-    info: {
-      nickname: 'nick',
-      image: 'http://lorempixel.com/400/200/'
-    }
-  })
+[:twitter, :weibo].each do |provider|
+  OmniAuth.config.mock_auth[provider] = OmniAuth::AuthHash.new({
+      provider: provider.to_s,
+      uid: '123545',
+      info: {
+        nickname: 'nick',
+        image: 'http://lorempixel.com/400/200/'
+      }
+    })
+end
