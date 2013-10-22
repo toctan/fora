196ab3a1b7f731d526a0896fbc89233cc5c507b0
toctan <tianjin.sc@gmail.com>
2013-10-22 12:45:09 +0800

    Refactor avatar_tag helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,50 +1,36 @@
 module ApplicationHelper
-  def avatar_tag(user, opts = {})
-    # TODO: method too long...
-    link = opts.include?(:link) ? opts[:link] : true
-    name = opts.include?(:name) ? opts[:name] : true
-    size =  opts[:size] || :normal
-    img_class  = opts[:img_class]
+  def avatar_tag(user, size = :normal, opts = {})
+    img = avatar_img(user, size, opts[:img_class])
 
-    img = avatar_img(user, size, img_class)
-
-    img << user.username if name
-
-    if link
-      link_to img, user_path(username: user.username),  class: 'name'
-    else
-      img
-    end
+    link_to img, '#', class: "name #{opts[:class]}"
   end
 
-  def font_icon(name, size = nil,  data = {})
-    klass = "icon #{name}"
-    klass << " icon-#{size}" if size
-    content_tag(:i, nil, class: klass, data: data, style: 'cursor:pointer')
+  def avatar_img(user, size, klass = 'rounded')
+    img_src =
+      if user.avatar?
+        user.avatar.url(size)
+      else
+        width = user_avatar_width_for_size(size)
+        gravatar_id = Digest::MD5.hexdigest(user.email.downcase)
+        "http://gravatar.com/avatar/#{gravatar_id}.png?s=#{width}&d=identicon"
+      end
+
+    image_tag(img_src, class: "#{klass} ui image")
+  end
+
+  def icon(name, klass = nil, data = {})
+    content_tag(:i, nil, class: "#{name} #{klass} icon", data: data)
   end
 
   private
 
   def user_avatar_width_for_size(size)
     case size
-    when :thumb  then 22
+    when :thumb  then 24
     when :normal then 40
     when :large  then 64
     else size
     end
-  end
-
-  def avatar_img(user, size, img_class)
-    width = user_avatar_width_for_size(size)
-    img_src =
-      if user.avatar?
-        user.avatar.url(size)
-      else
-        gravatar_id = Digest::MD5.hexdigest(user.email.downcase)
-        "http://gravatar.com/avatar/#{gravatar_id}.png?s=#{width}&d=identicon"
-      end
-
-    image_tag(img_src, class: "img-rounded #{img_class}")
   end
 
   def notice_message
