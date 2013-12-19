d78f365d602d66affa7a3a25f7f1ada1ac533705
toctan <tianjin.sc@gmail.com>
2013-12-19 18:52:55 +0800

    Delete notification with SJR

--- spec/features/replies/admin_delete_reply_spec.rb
+++ spec/features/replies/admin_delete_reply_spec.rb
@@ -1,12 +1,12 @@
 require 'spec_helper'
 
-feature 'Admin' do
+feature 'Admin', :admin do
   let(:topic) { create(:topic) }
   let!(:reply) { create(:reply, topic: topic) }
 
   before(:each) { visit topic_path(topic) }
 
-  scenario 'delete spam reply', :admin do
+  scenario 'delete spam reply', :js do
     find('.js-delete-reply').click
 
     expect(page).to have_selector('.reply', count: 0)
