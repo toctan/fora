21b28db88294c860925a326d315b876e489c5c6f
toctan <tianjin.sc@gmail.com>
2013-12-19 19:14:39 +0800

    Fix random fail bookmark spec

--- spec/features/topics/bookmark_topic_spec.rb
+++ spec/features/topics/bookmark_topic_spec.rb
@@ -4,12 +4,22 @@ feature 'Bookmark', :signin do
   let(:topic) { create(:topic) }
 
   scenario 'User bookmarks a topic successfully', :js do
-    # TODO: This test fails randomly, why?
     visit topic_path(topic)
     click_link 'js-bookmark'
+
+    wait_for_ajax
 
     visit bookmarks_path
 
     expect(page).to have_content(topic.title)
+  end
+end
+
+def wait_for_ajax
+  Timeout.timeout(Capybara.default_wait_time) do
+    loop do
+      active = page.evaluate_script('jQuery.active')
+      break if active == 0
+    end
   end
 end
